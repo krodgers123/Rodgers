48875594618d70576c125086aece7437f1096ab0
krodgers123 <145645963+krodgers123@users.noreply.github.com>
2024-03-15 13:04:38 +0300

    Add files via upload

---
+++ Act/Rodgers/prac.py
@@ -0,0 +1,88 @@
+# a = 5
+# print("Type of a: ", type(a)) 
+
+# b = 5.0
+# print("\nType of b: ", type(b)) 
+
+# c = 2 + 4j
+# print("\nType of c: ", type(c)) 
+
+
+# String1 = 'Welcome to the Geeks World'
+# print("String with the use of Single Quotes: ") 
+# print(String1) 
+# String1 = "I'm a Geek"
+# print("\nString with the use of Double Quotes: ") 
+# print(String1) 
+# print(type(String1)) 
+# String1 = '''I'm a Geek and I live in a world of "Geeks"'''
+# print("\nString with the use of Triple Quotes: ") 
+# print(String1) 
+# print(type(String1)) 
+
+# String1 = '''Geeks 
+# 			For  
+# 			     Life'''
+# print("\nCreating a multiline String: ") 
+# print(String1) 
+
+
+# List = [] 
+# print("Initial blank List: ") 
+# print(List) 
+# List = ['GeeksForGeeks'] 
+# print("\nList with the use of String: ") 
+# print(List) 
+# List = ["Geeks", "For", "Geeks"] 
+# print("\nList containing multiple values: ") 
+# print(List[0]) 
+# print(List[2]) 
+# List = [['Geeks', 'For'], ['Geeks']] 
+# print("\nMulti-Dimensional List: ") 
+# print(List) 
+
+
+
+# Q80. Write a program in Python to check if a sequence is a Palindrome.
+# a=input("enter sequence")
+# b=a[::-1]
+# if a==b:
+#   print("palindrome")
+# else:
+#   print("Not a Palindrome")
+
+
+# Q77. Write a program in Python to produce Star triangle.
+
+# def pyfunc(r):
+#     for x in range(r):
+#         print(' '(r-x-1)+''*(2*x+1))    
+# pyfunc(9)
+
+
+# Q82. Write a sorting algorithm for a numerical dataset in Python.
+# Ans: The following code can be used to sort a list in Python:
+
+
+# list = ["1", "4", "0", "6", "9"]
+# list = [int(i) for i in list]
+# list.sort()
+# print (list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
